3f2a0892b1db5eeaaf80e65f209a1c93ef5b4400
Nino Filiu <nino.filiu@gmail.com>
2021-11-19 02:39:30 +0100

    build

--- dist/index.js
+++ dist/index.js
@@ -7,3 +7,45 @@ export const getUserInput = async (fftSize) => {
     source.connect(analyser);
     return analyser;
 };
+export const createKickDetector = (analyser) => {
+    let kicking = false;
+    let listening = false;
+    const self = {
+        threshold: Infinity,
+        up: 0,
+        smooth: 0.8,
+        onKick: () => { },
+    };
+    const listen = () => {
+        if (listening)
+            return;
+        listening = true;
+        const data = new Uint8Array(analyser.frequencyBinCount);
+        const loop = () => {
+            if (!listening)
+                return;
+            const last = new Uint8Array(data);
+            analyser.getByteFrequencyData(data);
+            let newUp = 0;
+            for (let f = 0; f < data.length; f++)
+                newUp += Math.max(0, data[f] - last[f]) / data.length / 256;
+            self.up = self.smooth * self.up + (1 - self.smooth) * newUp;
+            if (self.up > self.threshold) {
+                if (!kicking) {
+                    kicking = true;
+                    self.onKick();
+                }
+            }
+            else {
+                kicking = false;
+            }
+            requestAnimationFrame(loop);
+        };
+        loop();
+    };
+    const stop = () => {
+        listening = false;
+        self.up = 0;
+    };
+    return Object.assign(self, { listen, stop });
+};
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,10 @@
 export declare const getUserInput: (fftSize: number) => Promise<AnalyserNode>;
+export declare const createKickDetector: (analyser: AnalyserNode) => {
+    threshold: number;
+    up: number;
+    smooth: number;
+    onKick: () => void;
+} & {
+    listen: () => void;
+    stop: () => void;
+};
